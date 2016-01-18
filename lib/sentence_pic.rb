664444de99535aafe4304545fab983239bb18728
Brian Whitmer <brian.whitmer@gmail.com>
2016-01-18 16:10:51 -0700

    more flexible rendering of utterance previews

--- lib/sentence_pic.rb
+++ lib/sentence_pic.rb
@@ -15,12 +15,24 @@ module SentencePic
       image_commands << "-label \"#{(button['label'] || button['vocalization'] || '').gsub("\"", "\\\"")}\" #{filename}"
     end
     montage = OBF::Utils.temp_path('montage') + '.png'
-    `montage #{image_commands.join(' ')} -tile #{PER_ROW}x -shadow -pointsize 16 -background white -geometry 70x70+3+10 #{montage}`
+    rows = (image_commands.length.to_f / PER_ROW.to_f).ceil
+    columns = PER_ROW
+    width = 70
+    height = 70
+    if image_commands.length < PER_ROW
+      columns = image_commands.length
+      width = [420 / columns, 200].min
+      height = width
+    elsif image_commands.length < PER_ROW * 2
+      columns = (image_commands.length.to_f / 2.0).ceil
+    end
+    
+    `montage #{image_commands.join(' ')} -tile #{columns}x#{rows} -shadow -pointsize 16 -geometry #{width}x#{height}+3+10 -border 2 -bordercolor "#888" #{montage}`
     preview = OBF::Utils.temp_path('preview') + '.png'
-    if image_commands.length > PER_ROW
-      `convert #{montage} -gravity top -extent 460x230 #{preview}`
+    if image_commands.length > PER_ROW * 2
+      `convert #{montage} -gravity north -extent 500x240 #{preview}`
     else
-      `convert #{montage} -gravity center -extent 460x230 #{preview}`
+      `convert #{montage} -gravity center -extent 500x240 #{preview}`
     end
     key = Security.sha512(utterance.id.to_s, 'utterance_id')[0, 25]
     remote_path = "sentences/#{utterance.id}/#{key}/preview.png"
