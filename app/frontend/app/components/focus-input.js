f4da678e021008dbedf4bd68dc23b4f1dd9ed580
Brian Whitmer <brian.whitmer@gmail.com>
2016-01-21 10:50:27 -0700

    don't auto-focus text fields on mobile, it's annoying

--- app/frontend/app/components/focus-input.js
+++ app/frontend/app/components/focus-input.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
+import capabilities from '../utils/capabilities';
 
 export default Ember.TextField.extend({
   becomeFocused: function() {
-    this.$().focus().select();
+    if(!capabilities.mobile) {
+      this.$().focus().select();
+    }
   }.on('didInsertElement'),
   focusOut: function() {
     this.sendAction();
