d892cd494352f38c3c8ea92118e5bb05604384dd
Brian Whitmer <brian.whitmer@gmail.com>
2016-01-21 11:21:30 -0700

    missed share image render spec

--- spec/lib/sentence_pic_spec.rb
+++ spec/lib/sentence_pic_spec.rb
@@ -19,8 +19,8 @@ describe SentencePic do
     expect(OBF::Utils).to receive(:save_image).with({'url' => 'http://www.example.com/pic.png'}).and_return("pic2.png")
     expect(OBF::Utils).to receive(:temp_path).with('montage').and_return('/tmp/montage')
     expect(OBF::Utils).to receive(:temp_path).with('preview').and_return('/tmp/preview')
-    expect(SentencePic).to receive(:'`').with("montage -label \"hat\" pic1.png -label \"cat\" pic1.png -label \"scat\" pic2.png -tile 6x -shadow -pointsize 16 -background white -geometry 70x70+3+10 /tmp/montage.png").and_return(nil)
-    expect(SentencePic).to receive(:'`').with("convert /tmp/montage.png -gravity center -extent 460x230 /tmp/preview.png")
+    expect(SentencePic).to receive(:'`').with("montage -label \"hat\" pic1.png -label \"cat\" pic1.png -label \"scat\" pic2.png -tile 3x1 -shadow -pointsize 16 -geometry 140x140+3+10 -border 2 -bordercolor \"#888\" /tmp/montage.png").and_return(nil)
+    expect(SentencePic).to receive(:'`').with("convert /tmp/montage.png -gravity center -extent 500x240 /tmp/preview.png")
     key = Security.sha512(u.id.to_s, 'utterance_id')[0, 25]
     expect(Uploader).to receive(:remote_upload).with("sentences/#{u.id}/#{key}/preview.png", "/tmp/preview.png", "image/png").and_return("http://www.example.com/pid.png")
     res = SentencePic.generate(u)
