b8c1d5232c335b3e79f753ddd49b6987fb3ac53f
Brian Whitmer <brian.whitmer@gmail.com>
2016-02-17 14:22:13 -0700

    more tracing for perf improvements

--- app/controllers/api/button_sets_controller.rb
+++ app/controllers/api/button_sets_controller.rb
@@ -3,8 +3,14 @@ class Api::ButtonSetsController < ApplicationController
   before_filter :require_api_token
   
   def show
-    board = Board.find_by_path(params['id'])
-    button_set = board && board.board_downstream_button_set
+    board = nil
+    button_set = nil
+    self.class.trace_execution_scoped(['button_set/board/lookup']) do
+      board = Board.find_by_path(params['id'])
+    end
+    self.class.trace_execution_scoped(['button_set/button_set/lookup']) do
+      button_set = board && board.board_downstream_button_set
+    end
     return unless exists?(button_set)
     allowed = false
     self.class.trace_execution_scoped(['button_set/board/permission_check']) do
@@ -12,9 +18,11 @@ class Api::ButtonSetsController < ApplicationController
     end
     return unless allowed
     json = {}
+    json_str = "null"
     self.class.trace_execution_scoped(['button_set/board/json_render']) do
       json = JsonApi::ButtonSet.as_json(button_set, :wrapper => true, :permissions => @api_user)
+      json_str = json.to_json
     end
-    render json: json.to_json
+    render json: json_str
   end
 end
