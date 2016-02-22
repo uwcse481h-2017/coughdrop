1ba49405482bde4e237604969a87241a3abdae89
Brian Whitmer <brian.whitmer@gmail.com>
2016-02-22 13:13:44 -0700

    move visible highlight

--- app/frontend/app/controllers/highlight.js
+++ app/frontend/app/controllers/highlight.js
@@ -17,13 +17,39 @@ export default modal.ModalController.extend({
       opacity = "0.0";
     }
     var header_height = Ember.$("header").outerHeight();
+    var window_height = Ember.$(window).height();
+    var window_width = Ember.$(window).width();
     var top = this.get('model.top');
+    var left = this.get('model.left');
     var bottom = this.get('model.bottom');
+    var right = this.get('model.right');
+    var width = this.get('model.width');
+    var height = this.get('model.height');
+    if(top < 4) {
+      height = height - (4 - top);
+      top = 4;
+    }
+    if(bottom > window_height - 4) {
+      height = height - (bottom - (window_height - 4));
+      bottom = window_height - 4;
+    }
+    if(left < 4) {
+      width = width - (4 - left);
+      left = 4;
+    }
+    if(right > window_width - 20) {
+      width = width - (right - (window_width - 4));
+      right = window_width - 4;
+    }
+    if(width > window_width - 8) {
+      width = window_width - 8;
+    }
     this.set('model.top_style', new Ember.Handlebars.SafeString(display + "z-index: 9; position: absolute; top: -" + header_height + "px; left: 0; background: #000; opacity: " + opacity + "; width: 100%; height: " + top + "px;"));
-    this.set('model.left_style', new Ember.Handlebars.SafeString(display + "z-index: 9; position: absolute; top: " + (top - header_height) + "px; left: 0; background: #000; opacity: " + opacity + "; width: " + this.get('model.left') + "px; height: " + this.get('model.height') + "px;"));
-    this.set('model.right_style', new Ember.Handlebars.SafeString(display + "z-index: 9; position: absolute; top: " + (top - header_height) + "px; left: calc(" + this.get('model.left') + "px + " + this.get('model.width') + "px); background: #000; opacity: " + opacity + "; width: calc(100% - " + this.get('model.left') + "px - " + this.get('model.width') + "px); height: " + this.get('model.height') + "px;"));
+    this.set('model.left_style', new Ember.Handlebars.SafeString(display + "z-index: 9; position: absolute; top: " + (top - header_height) + "px; left: 0; background: #000; opacity: " + opacity + "; width: " + left + "px; height: " + height + "px;"));
+    this.set('model.right_style', new Ember.Handlebars.SafeString(display + "z-index: 9; position: absolute; top: " + (top - header_height) + "px; left: calc(" + left+ "px + " + width + "px); background: #000; opacity: " + opacity + "; width: calc(100% - " + left + "px - " + width + "px); height: " + height + "px;"));
     this.set('model.bottom_style', new Ember.Handlebars.SafeString(display + "z-index: 9; position: absolute; top: " + (bottom - header_height) + "px; left: 0; background: #000; opacity: " + opacity + "; width: 100%; height: 5000px;"));
-    this.set('model.highlight_style', new Ember.Handlebars.SafeString("z-index: 10; position: absolute; top: " + (top - header_height) + "px; left: " + this.get('model.left') + "px; width: " + this.get('model.width') + "px; height: " + this.get('model.height') + "px; border: 4px solid #f00; cursor: pointer;"));
+    this.set('model.highlight_style', new Ember.Handlebars.SafeString("z-index: 10; position: absolute; top: " + (top - header_height - 4) + "px; left: " + (left - 4) + "px; width: " + (width + 8) + "px; height: " + (height + 8) + "px; cursor: pointer;"));
+    this.set('model.inner_highlight_style', new Ember.Handlebars.SafeString("z-index: 11; position: absolute; top: " + (top - header_height) + "px; left: " + left + "px; width: " + width + "px; height: " + height + "px; cursor: pointer;"));
   }.observes('model.left', 'model.top', 'model.width', 'model.height', 'model.bottom', 'model.right', 'model.overlay'),
   actions: {
     select: function() {
