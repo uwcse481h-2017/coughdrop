1d61c7b1e170efe8d3b4b81e3804aa6d9e983f93
Brian Whitmer <brian.whitmer@gmail.com>
2016-03-12 14:45:08 -0700

    don't error on typhoeus lookups when offline

--- app/models/board.rb
+++ app/models/board.rb
@@ -300,7 +300,8 @@ class Board < ActiveRecord::Base
   def check_image_url
     if self.settings && self.settings['image_url'] == DEFAULT_ICON && self.settings['default_image_url'] == self.settings['image_url'] && self.settings['name'] && self.settings['name'] != 'Unnamed Board'
       res = Typhoeus.get("https://www.opensymbols.org/api/v1/symbols/search?q=#{CGI.escape(self.settings['name'])}", :ssl_verifypeer => false)
-      results = JSON.parse(res.body)
+      results = JSON.parse(res.body) rescue nil
+      results ||= []
       icon = results.detect do |result|
         result['license'] == "CC By" || result['repo_key'] == 'arasaac'
       end
