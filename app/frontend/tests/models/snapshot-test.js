d15c7b114a64f1a25d9f3bf805abbf484804bc07
Brian Whitmer <brian.whitmer@gmail.com>
2016-06-28 11:40:04 -0600

    empty test (for now)

---
+++ app/frontend/tests/models/snapshot-test.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+import { test, moduleForModel } from 'ember-qunit';
+import { describe, it, expect, beforeEach, afterEach, waitsFor, runs, stub } from 'frontend/tests/helpers/jasmine';
+import { } from 'frontend/tests/helpers/ember_helper';
+import CoughDrop from '../../app';
+import speecher from '../../utils/speecher';
+import persistence from '../../utils/persistence';
+
+describe('Snapshot', function() {
+});
