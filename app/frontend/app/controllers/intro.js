830737759649ad50e763e83c8c943f101f1bc0ab
Brian Whitmer <brian.whitmer@gmail.com>
2016-09-27 13:33:54 -0600

    intro video tweaks

--- app/frontend/app/controllers/intro.js
+++ app/frontend/app/controllers/intro.js
@@ -9,6 +9,9 @@ export default modal.ModalController.extend({
     user.save().then(null, function() { });
     this.set('page', 1);
     this.set('total_pages', 14);
+    if(window.ga) {
+      return window.ga('send', 'event', 'Intro', 'start', 'Intro Modal Opened');
+    }
   },
   set_pages: function() {
     var page = this.get('page');
@@ -31,7 +34,10 @@ export default modal.ModalController.extend({
       this.set('page', page);
     },
     video: function() {
-      modal.open('inline-video', {video: {url: "https://www.youtube.com/embed/XhxTjVgzyK0?rel=0"}, hide_overlay: true});
+      if(window.ga) {
+        return window.ga('send', 'event', 'Intro', 'video', 'Intro Video Opened');
+      }
+      modal.open('inline-video', {video: {url: "https://www.youtube.com/embed/L55dBzfO1pU?rel=0"}, hide_overlay: true});
     }
   }
 });
