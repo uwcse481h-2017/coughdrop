cdbdc59165e10f6f982b4cacd942a31dab79acd5
Kelsie Haakenson <kchaak@uw.edu>
2017-02-14 01:14:55 -0800

    Fixed error with order of dom elements being created when also creating the slideout. When opening directly to speak mode, div.board was not yet created. Moved attributes for panel and menu for slideout to elements who will be ready at the time of the slideout's creation.

--- app/frontend/app/components/board-editor-slideout.js
+++ app/frontend/app/components/board-editor-slideout.js
@@ -10,21 +10,23 @@ export default Ember.Component.extend(InboundActions, {
   // Set up slideout element once the DOM has been built and elements are accessible.
   didInsertElement: function() {
     this._super();
-
     // Create slideout that will move the board over when opened during editing mode.
     var newSlideout = new window.Slideout({
-      'panel': Ember.$('.board')[0],
+      'panel': Ember.$('#slideout-main-panel')[0],
       'menu': Ember.$('#menu')[0],
       'padding': 256,
       'tolerance': 70
     });
 
+
     // Append this to the component.
     this.set('slideout', newSlideout);
 
     // Set the content of the slideout to appear below the header.
-    var headerHeight = Ember.$('header').css('height');
-    Ember.$('#menu').css('padding-top', headerHeight);
+    // When in edit-mode, the header is 70px wide.
+    // TODO: make this a constant, or dynamically decide based on just the 
+    // edit header.
+    Ember.$('#menu').css('padding-top', '70px');
   },
   actions: {
     // Open/close the slideout. Can be accessed by controllers of parent components
